1ce6af11dd509eb0e5708d850f0f46b43f30feee
Marcelo Morales <morales.374@osu.edu>
2025-08-25 17:47:59 -0400

    Updating into mdx

--- src/app/mdx-components.tsx
+++ src/app/mdx-components.tsx
@@ -1,6 +1,8 @@
 import type { MDXComponents } from 'mdx/types';
 import Typewriter from './components/Typewriter';
+import Publication from './components/Publication';
 
 export const mdxComponents: MDXComponents = {
   Typewriter,
+  Publication,
 };
--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -1,27 +1,13 @@
+'use client';
 import React from 'react';
-import Section from '../components/Section';
-import PublicationList from '../components/PublicationList';
+import ResearchContent from './Research.mdx'; // This imports the MDX as a React component
+import { mdxComponents } from '../mdx-components';
 
 const Research: React.FC = () => {
   return (
-    <div className='min-h-screen p-8 sm:p-20 text-neutral-7'>
-      <h1 className='text-4xl font-bold mb-8 text-neutral-8'>Research</h1>
-
-      {/* Research Interests Section */}
-      <Section title='Research Interests'>
-        <p className='text-md text-neutral-6 mb-6 leading-relaxed tracking-wide'>
-          My research focuses on the intersection of cybersecurity and
-          blockchain technology. I’m particularly interested in areas such as
-          smart contract security, threat modeling, and machine learning
-          applications for detecting cyber threats.
-        </p>
-      </Section>
-
-      {/* Publications Section */}
-      <Section title='Publications'>
-        <PublicationList />
-      </Section>
-    </div>
+    <article className='prose prose-invert mx-auto p-4'>
+      <ResearchContent components={mdxComponents} />
+    </article>
   );
 };
 
